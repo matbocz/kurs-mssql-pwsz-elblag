e857dd58d3487fa7339a77906ce16eb3cf9ab7d2
Mateusz Boczarski <matbocz@outlook.com>
2018-11-03 16:14:55 +0100

    Add new queries

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,3 +4,11 @@ SELECT * FROM pracownik;
 SELECT imie FROM pracownik;
 --1.3 Wyswietlic zawartosc kolumn imie, nazwisko i dzial z tabeli pracownik.
 SELECT imie, nazwisko, dzial FROM pracownik;
+
+--2.1 Wyswietlic zawartosc kolumn imie, nazwisko i pensja z tabeli pracownik. Wynik posortuj malejaco wzgledem pensji.
+SELECT imie, nazwisko, pensja FROM pracownik ORDER BY pensja DESC;
+--2.2 Wyswietl zawartosc kolumn nazwisko i imie z tabeli pracownik. Wynik posortuj rosnaco (leksykograficznie) wzgledem nazwiska i imienia.
+SELECT nazwisko, imie FROM pracownik ORDER BY nazwisko ASC, imie ASC;
+--2.3 Wyswietlic zawartosc kolumn nazwisko, dzial, stanowisko z tabeli pracownik. Wynik posortuj rosnaco wzgledem dzialu, a dla tych samych nazw dzialow malejaco wzgledem stanowiska.
+SELECT nazwisko, dzial, stanowisko FROM pracownik ORDER BY dzial ASC, stanowisko DESC;
+
