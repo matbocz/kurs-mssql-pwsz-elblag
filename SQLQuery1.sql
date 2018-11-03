e8bf6499b3b552c1d9ff8be4aab3ec96ac57725b
Mateusz Boczarski <matbocz@outlook.com>
2018-11-03 15:58:00 +0100

    Create SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+--1.1 Wyswietlic zawartosc wszystkich kolumn z tabeli pracownik.
+SELECT * FROM pracownik;
+--1.2 Z tabeli pracownik wyswietlic same imiona pracownikow.
+SELECT imie FROM pracownik;
+--1.3 Wyswietlic zawartosc kolumn imie, nazwisko i dzial z tabeli pracownik.
+SELECT imie, nazwisko, dzial FROM pracownik;
