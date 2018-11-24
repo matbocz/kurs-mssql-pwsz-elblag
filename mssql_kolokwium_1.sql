fb4166b1bc9afd7a85d9de58ede19e915a695d0e
Mateusz Boczarski <matbocz@outlook.com>
2018-11-24 20:17:28 +0100

    Add new queries

--- mssql_kolokwium_1.sql
+++ mssql_kolokwium_1.sql
@@ -7,8 +7,14 @@ ORDER BY COUNT(k.id_adres) DESC, a.miejscowosc ASC;
 
 --2 Znajdz nazwy producentow (tabela producent), za ktorych towary klienci zaplacili ponad 50 tys. zl. brutto.
 --  Wyswietl tylko posortowane alfabetycznie nazwy producentow.
+SELECT p.nazwa
+FROM producent p JOIN produkt pr ON p.id_producent = pr.id_producent
+JOIN koszyk k ON pr.id_produkt = k.id_produkt
+GROUP BY p.nazwa, k.cena_netto, k.podatek, k.ilosc_sztuk
+HAVING (k.cena_netto + ((k.podatek / 100) * k.cena_netto)) * k.ilosc_sztuk > 50000;
 
 --3 Usun producentow, ktorzy nie maja zadnego produktu.
+DELETE FROM producent WHERE id_producent NOT IN(SELECT DISTINCT id_producent FROM produkt);
 
 --4 Dla kazdego roku (tabela zamowienie, kolumna data_zamowienia), oblicz ile bylo zlozonych zamowien.
 --  Wynik posortuj rosnaco wzgledem roku. Uzyj funkcji YEAR.
